b547e7712fd7cd40bf24c4954b1f63b34220d4da
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-04-17 16:53:31 -0400

    made almost everything pug

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,10 @@
 module.exports = function (eleventyConfig) {
-  // Passthroughs
-  eleventyConfig.addPassthroughCopy({
-    "src/img": "assets/img"
-  });
-  eleventyConfig.addPassthroughCopy({
-    "src/css": "assets/css"
-  });
-  eleventyConfig.addPassthroughCopy({
-    "src/scripts": "assets/scripts"
-  });
-  eleventyConfig.addPassthroughCopy({
-    "src/audio": "assets/audio"
-  });
-  eleventyConfig.addPassthroughCopy({
-    "src/video": "assets/video"
-  });
+    // Passthroughs
+    eleventyConfig.addPassthroughCopy('assets');
+
+    return {
+        dir: {
+            input: "src"
+        }
+    }
 };
