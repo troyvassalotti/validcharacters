3e0c392ad234a75bde1bc5435d0adccfb537500d
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2021-04-17 17:12:57 -0400

    fix passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
     // Passthroughs
-    eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.addPassthroughCopy('src/assets');
 
     return {
         dir: {
